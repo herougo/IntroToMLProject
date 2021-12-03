5986e46fbd7bbaae446c9627cd4ec8eb0a25c7cc
Henri R <herougo@gmail.com>
2021-12-03 13:30:05 -0800

    Learning a's, b's, and c's directly from the input works

--- ml/experiments/common/memory_network_model.py
+++ ml/experiments/common/memory_network_model.py
@@ -74,14 +74,26 @@ def questions_to_questions_onehot_tensor(questions, vocab_size):
 
 
 class MemoryNetwork(nn.Module):
-    def __init__(self, sentence_len, vocab_size, n_sentence_line_types, n_question_line_types, use_cuda):
+    def __init__(self, sentence_len, vocab_size, n_sentence_line_types, n_question_line_types, use_cuda,
+                 learn_abc_from_input=False, embedding_dim=100):
         super().__init__()
         self.n_sentence_line_types = n_sentence_line_types
         self.n_question_line_types = n_question_line_types
         self.vocab_size = vocab_size
-        self.a = Parameter(torch.zeros((n_sentence_line_types, sentence_len, sentence_len), requires_grad=True))
-        self.b = Parameter(torch.zeros((n_sentence_line_types, sentence_len), requires_grad=True))
-        self.c = Parameter(torch.zeros((n_question_line_types, sentence_len), requires_grad=True))
+        self.learn_abc_from_input = learn_abc_from_input
+        if learn_abc_from_input:
+            self.embedding_dim = embedding_dim
+            self.sent_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+            self.ab_hidden = nn.Linear(sentence_len * embedding_dim, sentence_len * embedding_dim)
+            self.a_head = nn.Linear(sentence_len * embedding_dim, sentence_len * sentence_len)
+            self.b_head = nn.Linear(sentence_len * embedding_dim, sentence_len)
+            self.question_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+            self.c_hidden = nn.Linear(sentence_len * embedding_dim, sentence_len * embedding_dim)
+            self.c_head = nn.Linear(sentence_len * embedding_dim, sentence_len)
+        else:
+            self.a = Parameter(torch.zeros((n_sentence_line_types, sentence_len, sentence_len), requires_grad=True))
+            self.b = Parameter(torch.zeros((n_sentence_line_types, sentence_len), requires_grad=True))
+            self.c = Parameter(torch.zeros((n_question_line_types, sentence_len), requires_grad=True))
         self.use_cuda = use_cuda
     
     def forward(self, sentence_sequences, questions, sentence_line_types, question_line_types, story_lengths):
@@ -112,11 +124,18 @@ class MemoryNetwork(nn.Module):
         b_s = []
 
         for i in range(seq_len):
-            a = self.a.view(1, self.n_sentence_line_types, sentence_len, sentence_len, 1, 1).repeat(batch_size, 1, 1, 1, 1, 1)
-            a = a[arange, sentence_line_types[:, i]]
+            if self.learn_abc_from_input:
+                emb = self.sent_emb(sentence_sequences[:, i]).view(batch_size, self.embedding_dim * sentence_len)
+                ab_hidden = self.ab_hidden(emb)
+                ab_hidden = torch.relu(ab_hidden)
+                a = self.a_head(ab_hidden).view(batch_size, sentence_len, sentence_len, 1, 1)
+                b = self.b_head(ab_hidden).view(batch_size, sentence_len, 1, 1)
+            else:
+                a = self.a.view(1, self.n_sentence_line_types, sentence_len, sentence_len, 1, 1).repeat(batch_size, 1, 1, 1, 1, 1)
+                a = a[arange, sentence_line_types[:, i]]
+                b = self.b.view(1, self.n_sentence_line_types, sentence_len, 1, 1).repeat(batch_size, 1, 1, 1, 1)
+                b = b[arange, sentence_line_types[:, i]]
             a_s.append(a)
-            b = self.b.view(1, self.n_sentence_line_types, sentence_len, 1, 1).repeat(batch_size, 1, 1, 1, 1)
-            b = b[arange, sentence_line_types[:, i]]
             b_s.append(b)
             write_to_memory = torch.sum(torch.tanh(a) * batch_onehot_tensor[:, i], axis=[1, 2])
             modified_identity = identity - torch.minimum(torch.sum(torch.sigmoid(b) * batch_onehot_tensor2[:, i], axis=1), ones)
@@ -126,9 +145,15 @@ class MemoryNetwork(nn.Module):
 
         memory_view = memory.view(batch_size, 1, self.vocab_size, self.vocab_size).repeat(1, sentence_len, 1, 1)
 
-        c = self.c.view(1, self.n_question_line_types, sentence_len).repeat(batch_size, 1, 1)
-        c = c[arange, question_line_types]  # batch_size x sentence_len
-        c = c.view(batch_size, sentence_len, 1, 1)
+        if self.learn_abc_from_input:
+            emb = self.question_emb(questions).view(batch_size, self.embedding_dim * sentence_len)
+            c_hidden = self.c_hidden(emb)
+            c_hidden = torch.relu(c_hidden)
+            c = self.c_head(c_hidden).view(batch_size, sentence_len, 1, 1)
+        else:
+            c = self.c.view(1, self.n_question_line_types, sentence_len).repeat(batch_size, 1, 1)
+            c = c[arange, question_line_types]  # batch_size x sentence_len
+            c = c.view(batch_size, sentence_len, 1, 1)
 
         mem_onehot_matmul = torch.bmm(
             memory_view.transpose(2, 3).view(-1, self.vocab_size, self.vocab_size),
@@ -149,9 +174,11 @@ class MemoryNetwork(nn.Module):
 # maps dict to dict, includes metrics and loss
 class MemoryNetworkModel(nn.Module):
     def __init__(self, sentence_len, vocab_size, n_sentence_line_types, n_question_line_types, use_cuda,
-                 word_map, use_interpretability=False, linear_layer_output=False):
+                 word_map, use_interpretability=False, linear_layer_output=False,
+                 learn_abc_from_input=False, embedding_dim=100):
         super(MemoryNetworkModel, self).__init__()
-        self.net = MemoryNetwork(sentence_len, vocab_size, n_sentence_line_types, n_question_line_types, use_cuda)
+        self.net = MemoryNetwork(sentence_len, vocab_size, n_sentence_line_types, n_question_line_types, use_cuda,
+                                 learn_abc_from_input=learn_abc_from_input, embedding_dim=embedding_dim)
         self.linear_layer_output = linear_layer_output
         if linear_layer_output:
             self.linear = nn.Linear(vocab_size, vocab_size)
