6a2226b375067517a0bdeba6e459a63aec136250
Henri R <herougo@gmail.com>
2021-12-03 18:03:19 -0800

    copied experiments folder

--- ml/experiments/babi1/main.py
+++ ml/experiments/babi1/main.py
@@ -1,5 +1,5 @@
 from ml.utils.config import process_command_line_and_get_config
-from ml.experiments.babione.core import MemoryNetAgent
+from ml.experiments.common.core import MemoryNetAgent
 from ml.utils.log import get_stream_debug_logger
 
 logger = get_stream_debug_logger('Memory_Network_bAbI_1')
---
+++ ml/experiments/babi2/main.py
@@ -0,0 +1,20 @@
+from ml.utils.config import process_command_line_and_get_config
+from ml.experiments.common.core import MemoryNetAgent
+from ml.utils.log import get_stream_debug_logger
+
+logger = get_stream_debug_logger('Memory_Network_bAbI_1')
+
+
+def main():
+    config = process_command_line_and_get_config()
+    logger.info(f'Config: {config}')
+    agent = MemoryNetAgent(config, logger, dir_include_date=True)
+    agent.setup()
+    agent.train()
+    print('Running on Test Data')
+    agent.model.use_interpretability = True
+    print(agent.test())
+
+
+if __name__ == '__main__':
+    main()
