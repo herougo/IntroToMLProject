00a4f6ebd34aa19662906454fa086d309dd3748e
Henri R <herougo@gmail.com>
2021-12-03 18:12:22 -0800

    Fixed config files for babi tasks 1, 6, 10, and 12

--- ml/experiments/babi2/main.py
+++ ml/experiments/babi2/main.py
@@ -2,7 +2,7 @@ from ml.utils.config import process_command_line_and_get_config
 from ml.experiments.common.core import MemoryNetAgent
 from ml.utils.log import get_stream_debug_logger
 
-logger = get_stream_debug_logger('Memory_Network_bAbI_1')
+logger = get_stream_debug_logger('Memory_Network_bAbI_2')
 
 
 def main():
